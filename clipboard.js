88284bc1502cf9b0dac8ec00f1d4ddbeaca91b4e
julias-del <julias@repli360.com>
2026-06-30 14:02:37 -0400

    updated files

---
+++ clipboard.js
@@ -0,0 +1,12 @@
+export async function copyText(text) {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch {
+    const el = document.createElement("textarea");
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  }
+}
